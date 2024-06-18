2f51421f696755286cdc7b9cf4b54eaef54589f9
rahul.kolawale <rahul.kolawale@MYSHEC544521L>
2024-06-18 15:05:33 +0530

    Add project files.

---
+++ Infosys.DBFirstCore.DataAccessLayer/QuickKartRepository.cs
@@ -0,0 +1,139 @@
+﻿using Infosys.DBFirstCore.DataAccessLayer.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infosys.DBFirstCore.DataAccessLayer
+{    public class QuickKartRepository
+    {
+        private readonly QuickKartDbContext context;
+        public QuickKartRepository(QuickKartDbContext context)
+        {
+            this.context=context;
+        }
+
+        public List<Category> GetAllCategories()
+        {
+            List<Category> categories = new List<Category>();
+            categories=(from c in context.Categories orderby c.CategoryId select c).ToList();
+            return categories;
+        }
+
+        public List<Product> GetProductOnCategoryId(byte categoryId)
+        {
+            List<Product> listOfProduct = new List<Product> ();
+            try
+            {
+                listOfProduct = (from p in context.Products where p.CategoryId == categoryId orderby p.Price ascending select p).ToList();
+            }
+            catch (Exception)
+            {
+
+                listOfProduct = null;
+            }
+            return listOfProduct;
+        }
+
+        public Product FilterProduct(byte categoryId)
+        {
+            Product product=new Product();
+           
+                product = (from p in context.Products where p.CategoryId == categoryId orderby p.Price ascending select p).LastOrDefault();
+            
+            return product;
+        }
+
+        public List<Product> FilterProductUsingLike(string pattern)
+        {
+            List<Product> lstOfProduct = new List<Product>();
+            try
+            {
+               lstOfProduct=(from p in context.Products where EF.Functions.Like(p.ProductName,pattern) select p).ToList();
+            }
+            catch (Exception)
+            {
+                lstOfProduct = null;
+            }
+            return lstOfProduct;
+        }
+
+        public List<User> GetAllUser()
+        {
+            List<User> lstOfUser = new List<User>();
+            try
+            {
+                lstOfUser = (from u in context.Users select u).ToList();
+            }
+            catch (Exception)
+            {
+
+                lstOfUser=null;
+            }
+            return lstOfUser;
+        }
+
+        //public int AddCategoryDetailsUsingUSP(string categoryName,out byte categoryId) 
+        //{ 
+        //    categoryId=0;
+        //    int noOfRowsaffected=0;
+        //    int returnResult = 0;
+        //    SqlParameter prmCategoryName= new SqlParameter("@CategoryName",categoryName);
+        //    SqlParameter prmCategoryId = new SqlParameter("@CategoryId", System.Data.SqlDbType.TinyInt);
+        //    prmCategoryId.Direction = System.Data.ParameterDirection.Output;
+        //    SqlParameter prmReturnResult = new SqlParameter("@ReturnResult", System.Data.SqlDbType.Int);
+        //    prmReturnResult.Direction = System.Data.ParameterDirection.Output;
+        //    //prmCategoryName.ParameterName = "@CategoryName";
+        //    //prmCategoryName.Value = categoryName;
+        //    //prmCategoryName.SqlDbType = System.Data.SqlDbType.VarChar;
+        //    //prmCategoryName.Size = 80;
+        //    //prmCategoryName.Direction=System.Data.ParameterDirection.Input;
+        //    try
+        //    {
+        //        noOfRowsaffected=context.Database.ExecuteSqlRaw("EXEC @ReturnResult=usp_AddCategory @CategoryName @CategoryId OUT", prmReturnResult, prmCategoryName, prmCategoryId);
+        //        returnResult=Convert.ToInt32(prmReturnResult.Value);
+        //        categoryId = Convert.ToByte(prmCategoryId.Value);
+        //    }
+        //    catch (Exception)
+        //    {
+
+        //        categoryId=0;
+        //        noOfRowsaffected = -1;
+        //        returnResult = -99;
+        //    }
+        //    return returnResult;
+        //}
+
+        public int AddCategoryDetailsUsingUSP(string categoryName, out byte categoryId)
+        {
+            categoryId = 0;
+            int noOfRowsAffected = 0;
+            int returnResult = 0;
+            SqlParameter prmCategoryName = new SqlParameter("@CategoryName", categoryName);
+            SqlParameter prmCategoryId = new SqlParameter("@CategoryId", System.Data.SqlDbType.TinyInt);
+            prmCategoryId.Direction = System.Data.ParameterDirection.Output;
+            SqlParameter prmReturnResult = new SqlParameter("@ReturnResult", System.Data.SqlDbType.Int);
+            prmReturnResult.Direction = System.Data.ParameterDirection.Output;
+            try
+            {
+                noOfRowsAffected = context.Database
+                                .ExecuteSqlRaw("EXEC @ReturnResult = usp_AddCategory @CategoryName, @CategoryId OUT",
+                                               prmReturnResult, prmCategoryName, prmCategoryId);
+                returnResult = Convert.ToInt32(prmReturnResult.Value);
+
+                categoryId = Convert.ToByte(prmCategoryId.Value);
+            }
+            catch (Exception)
+            {
+                categoryId = 0;
+                noOfRowsAffected = -1;
+                returnResult = -99;
+            }
+            return returnResult;
+        }
+
+    }
+}
---
+++ Infosys.QuickKartDBFirst.ConsoleApp/Program.cs
@@ -0,0 +1,97 @@
+﻿using Infosys.DBFirstCore.DataAccessLayer;
+using Infosys.DBFirstCore.DataAccessLayer.Models;
+
+namespace Infosys.QuickKartDBFirst.ConsoleApp
+{
+    public class Program
+    {
+        static QuickKartRepository repository;
+        static QuickKartDbContext context;
+        static Program()
+        {
+            context = new QuickKartDbContext();
+            repository=new QuickKartRepository(context);
+        }
+
+        static void Main(string[] args)
+        {
+            //var categories=repository.GetAllCategories();
+            //foreach(var category in categories)
+            //{
+            //    Console.WriteLine("{0}\t\t{1}", category.CategoryId, category.CategoryName);
+            //}
+            //byte categoryId = 1;
+            //List<Product> lstproducts = repository.GetProductOnCategoryId(categoryId);
+            //if (lstproducts.Count == 0)
+            //{
+            //    Console.WriteLine("No products available under the category = " + categoryId);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4}", "ProductId", "ProductName", "CategoryId", "Price", "QuantityAvailable");
+            //    Console.WriteLine("---------------------------------------------------------------------------------------");
+            //    foreach (var product in lstproducts)
+            //    {
+            //        Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4}", product.ProductId, product.ProductName, product.CategoryId, product.Price, product.QuantityAvailable);
+            //    }
+            //}
+
+            //Product product = repository.FilterProduct(categoryId);
+            //if (product == null)
+            //{
+            //    Console.WriteLine("No product details available");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4}", "ProductId", "ProductName", "CategoryId", "Price", "QuantityAvailable");
+            //    Console.WriteLine("---------------------------------------------------------------------------------------");
+            //    Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4}", product.ProductId, product.ProductName, product.CategoryId, product.Price, product.QuantityAvailable);
+            //}
+
+            //string pattern="BMW%";
+            //List<Product> p = new List<Product>();
+            //p = repository.FilterProductUsingLike(pattern);
+            //if (p.Count == 0 ) {
+            //    Console.WriteLine("No pattern matching product found");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4}", "ProductId", "ProductName", "CategoryId", "Price", "QuantityAvailable");
+            //    Console.WriteLine("---------------------------------------------------------------------------------------");
+            //    foreach (var product in p)
+            //    {
+            //        Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4}", product.ProductId, product.ProductName, product.CategoryId, product.Price, product.QuantityAvailable);
+            //    }
+            //}
+
+            //List<User> u=repository.GetAllUser();
+            //if(u.Count==0)
+            //{
+            //    Console.WriteLine("No users available");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4,-15}{4}", "Email", "UserPassword", "RoleId", "Gender", "Address");
+            //    Console.WriteLine("---------------------------------------------------------------------------------------");
+            //    foreach (var product in u)
+            //    {
+            //        Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4,-15}{4}", product.EmailId, product.UserPassword, product.RoleId, product.Gender, product.Address);
+            //    }
+            //}
+
+
+            byte categoryId = 0;
+            int returnResult = repository.AddCategoryDetailsUsingUSP("Footwear", out categoryId);
+            if (returnResult > 0)
+            {
+                Console.WriteLine("Category details added successfully with CategoryId = " + categoryId);
+                
+            }
+            else
+            {
+                Console.WriteLine("Some error occurred. Try again!");
+                
+            }
+        }
+    }
+}
